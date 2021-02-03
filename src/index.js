a6fc8f704c5d5bddecd42a306dd3885dd20daac1
alif <alif@admin.com>
2021-02-03 15:37:36 +0600

    assr

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import aws_export from './aws-exports';
+
+
+Amplify.configure(aws_export)
 
 ReactDOM.render(
   <React.StrictMode>
