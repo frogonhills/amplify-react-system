9796b15811222de4a8f49d42e008c26a02e5a3b8
alif <alif@admin.com>
2021-02-05 00:00:29 +0600

    allors

--- src/components/DisplayPost.js
+++ src/components/DisplayPost.js
@@ -1,12 +1,51 @@
 import React , { Component } from 'react';
+import {listPosts} from '../graphql/queries';
+import {API , graphqlOperation} from 'aws-amplify';
 
 
 
 class DisplayPost extends Component {
+
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount = async() => {
+        this.getPosts()
+    }
+
+
+    getPosts = async () =>{
+
+        const result = await API.graphql(graphqlOperation(listPosts))
+        //console.log("all posts" , JSON.stringify(result.data.listPosts.items))
+
+        this.setState({posts: result.data.listPosts.items})
+
+    }
+
     render(){
-        return(
-            <h1>Hello</h1>
-        )
+
+        const {posts} = this.state
+        return posts.map((post) => {
+            return(
+                <div className="posts" key={post.id}>
+                    <h1>{post.postTitle}</h1>
+                    <span>
+                        {"wrote by: "} {post.postOwnerUsername}
+                        {"on"}
+                        <time>
+
+                            {" "}
+                            {new Date(post.createdAt).toDateString()}
+
+                        </time>
+                    </span>
+                    <p>{post.postBody}</p>
+                </div>
+            )
+        })
     }
 }
 
