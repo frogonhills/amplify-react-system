cd4bb9da41ce094e410c119a6ae58f5460cedeb5
alif <alif@admin.com>
2021-02-04 19:14:40 +0600

    assr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import DisplayPost from './components/DisplayPost';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       
        
       <h1> অন্তরজ্বালা  </h1>
+      <DisplayPost />
     </div>
   );
 }
